9a6da32ad26e66f3e048b7dfe520af37577692d8
brendamesias <brendamesiasvillar@gmail.com>
2017-12-30 14:13:59 -0500

    data.js finiched

---
+++ js/data.js
@@ -0,0 +1,64 @@
+var tiposDeComidas = {
+  'comida japonesa': {
+    'platos': [
+      {
+        name: 'pollo terikayi',
+        photo: 'https://www.recetin.com/wp-content/uploads/2014/10/pollo_teriyaki.jpg',
+      },
+      {
+        name: 'sashimi japon',
+        photo: 'https://www.manusmenu.com/wp-content/uploads/2016/05/1-Salmon-Sashimi-with-Ponzu-3-1-of-1.jpg',
+      },
+      {
+        name: 'sopa ramen',
+        photo: 'https://www.orientalmarket.es/wp-content/uploads/2016/07/ramen-1024x683.jpeg',
+      },
+      {
+        name: 'sushi',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/71nYq1V-LQL._SL1280_.jpg',
+      },
+    ]
+  },
+  'comida peruana': {
+    'platos': [
+      {
+        name: 'aji de gallina',
+        photo: 'http://1.bp.blogspot.com/-BMTCHsBQfM0/Uw9ornUaCQI/AAAAAAAABCg/qJdHlz_K-sI/s1600/aji_gallina.jpg',
+      },
+      {
+        name: 'arroz con pollo',
+        photo: 'http://3.bp.blogspot.com/-yHnIgqgE5XE/Uw9orpqMgsI/AAAAAAAABCk/J4WRvawIE_k/s1600/arroz_pollo.jpg',
+      },
+      {
+        name: 'ceviche',
+        photo: 'https://t2.rg.ltmcdn.com/es/images/7/4/1/img_ceviche_peruano_18147_600.jpg',
+      },
+      {
+        name: 'lomo saltado',
+        photo: 'http://images-gmi-pmc.edge-generalmills.com/64f291cd-4e9c-47ca-af38-cebf1214b177.jpg',
+      },
+    ]
+  },
+  'comida mexicana': {
+    'platos': [
+      {
+        name: 'chiles en nogada',
+        photo: 'http://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2016/07/tumblrncsso8dyer1sl632qo101280.jpg',
+      },
+      {
+        name: 'chili nachos',
+        photo: 'http://www.hormel.com/Hormel/media/Hormel_Images/Recipes/ChiliNachos.jpg?width=242&height=242&ext=.jpg',
+      },
+      {
+        name: 'enchilada',
+        photo: 'https://www.recetin.com/wp-content/uploads/2014/10/pollo_teriyaki.jpg',
+      },
+      {
+        name: 'tacos',
+        photo: 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/10/1/RX-Rotel_Beef-Enchiladas-Rancheras_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371609442565.jpeg',
+      },
+    ]
+  },
+};
+
+console.log('hola mundo');
