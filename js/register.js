e2ee45fd610f4e3b45ab7d926da206be6d7b80af
brendamesias <brendamesiasvillar@gmail.com>
2018-01-03 16:06:08 -0500

    agregando modal

--- js/register.js
+++ js/register.js
@@ -1,21 +1,17 @@
 $(document).ready(function() {
-
-  $('#enviar').click(function(e) {
-    e.preventDefault();
+  $('#enviar').click(function(event) {
+    event.preventDefault();
+    $('#fotos_comidas').children().remove();
     let valorDeInput = $('#buscar_comida').val();
-
-    for(let i = 0; i < tiposDeComida.length; i++) {
+    for (let i = 0; i < tiposDeComida.length; i++) {
       if (tiposDeComida[i].name === valorDeInput) {
         let valor = tiposDeComida[i][valorDeInput]['platos'];
         valor.forEach(function(elemento) {
           let valorImg = elemento.photo;
-          $('#fotos_comidas').append(`<img src=${valorImg} class = 'border_radius'/>`);
-
+          $('#fotos_comidas').append(`<img src=${valorImg} class = 'border_radius' data-toggle="modal" data-target="#modal"/>`);
           $('.imagen_comida').addClass('display_none');
         });
       }
     }
-
-  })
-
+  });
 });
